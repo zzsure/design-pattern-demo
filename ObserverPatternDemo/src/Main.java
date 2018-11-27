935462b0c3ee7e18ce10c3b2dea26dad7367d632
zzs <zzspku@qq.com>
2018-11-27 17:09:04 +0800

    add observer pattern

---
+++ ObserverPatternDemo/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Subject subject = new Subject();
+
+        new BinaryObserver(subject);
+        new OctalObserver(subject);
+        new HexaObserver(subject);
+
+        System.out.println("First state change: 15");
+        subject.setState(15);
+
+        System.out.println("Second state change: 10");
+        subject.setState(10);
+    }
+}
