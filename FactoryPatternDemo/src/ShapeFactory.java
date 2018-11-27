8af1122b6363dfe4b03e2eefe9222486f80c39db
zzs <zzspku@qq.com>
2018-11-27 10:00:04 +0800

    add factory pattern demo

---
+++ FactoryPatternDemo/src/ShapeFactory.java
@@ -0,0 +1,15 @@
+public class ShapeFactory {
+    public Shape getShape(String shapeType) {
+        if (null == shapeType) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+        return null;
+    }
+}
