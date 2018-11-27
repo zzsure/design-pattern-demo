413c00815517ccdaee9b06b82eeb03b0e1dd7b9e
zzs <zzspku@qq.com>
2018-11-27 11:39:42 +0800

    add adapter pattern

---
+++ SingletonPatternDemo/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        SingleObject instance = SingleObject.getInstance();
+        instance.showMessage();
+    }
+}
---
+++ SingletonPatternDemo/src/SingleObject.java
@@ -0,0 +1,10 @@
+public class SingleObject {
+    private static SingleObject instance = new SingleObject();
+    private SingleObject() {}
+    public static SingleObject getInstance() {
+        return instance;
+    }
+    public void showMessage() {
+        System.out.println("Hello World!");
+    }
+}
